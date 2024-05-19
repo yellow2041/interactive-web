c3f01b5405d913de961dd4d54e92ee3cf1cc0360
yellow2041 <jeeyeon654@gmail.com>
2024-05-19 21:45:11 +0900

    section 11 스크롤 기능 구현

--- js/wall3d.js
+++ js/wall3d.js
@@ -1,7 +1,21 @@
-(function (){
-  const houseElem = document.querySelector('.house');
+(function () {
+  const houseElem = document.querySelector(".house");
+  const barElem = document.querySelector(".progress-bar");
+  let maxScrollValue = 0;
 
-  window.addEventListener('scroll',()=>{
-    console.log(scrollY);
-  })
+  const resizeHandler = () => {
+    maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  };
+
+  window.addEventListener("scroll", () => {
+    const scrollPer = scrollY / maxScrollValue;
+    const zMove = scrollPer * 980 - 490;
+    houseElem.style.transform = "translateZ(" + zMove + "vw)";
+
+    // progress bar
+    barElem.style.width = scrollPer * 100 + "%";
+  });
+
+  window.addEventListener("resize", resizeHandler);
+  resizeHandler();
 })();
