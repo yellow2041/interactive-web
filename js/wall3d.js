6c00a56889d2fc77e661d5e054cd0e5afecfbbce
yellow2041 <jeeyeon654@gmail.com>
2024-05-20 23:54:40 +0900

    section11 mouseover 이벤트 적용

--- js/wall3d.js
+++ js/wall3d.js
@@ -1,6 +1,8 @@
 (function () {
   const houseElem = document.querySelector(".house");
   const barElem = document.querySelector(".progress-bar");
+  const stageElem = document.querySelector(".stage");
+  const mousePos = { x: 0, y: 0 };
   let maxScrollValue = 0;
 
   const resizeHandler = () => {
@@ -14,6 +16,13 @@
 
     // progress bar
     barElem.style.width = scrollPer * 100 + "%";
+  });
+
+  window.addEventListener("mousemove", (e) => {
+    mousePos.x = -1 + (e.clientX / window.innerHeight) * 2;
+    mousePos.y = 1 - (e.clientY / window.innerHeight) * 2;
+    stageElem.style.transform =
+      "rotateX(" + mousePos.y * 5 + "deg) rotateY(" + mousePos.x * 5 + "deg";
   });
 
   window.addEventListener("resize", resizeHandler);
