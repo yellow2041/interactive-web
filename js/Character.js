e097b9c084732ce6212911395a3da76ae15f9fdd
yellow2041 <jeeyeon654@gmail.com>
2024-06-06 23:33:29 +0900

    스크롤 멈추면 일분이 러닝 멈추기 구현

--- js/Character.js
+++ js/Character.js
@@ -28,6 +28,7 @@ function Character({ xPos }) {
   document.querySelector(".stage").appendChild(this.mainElem);
 
   this.mainElem.style.left = xPos + "%";
+  this.scrollState = false;
   this.init();
 }
 
@@ -36,7 +37,19 @@ Character.prototype = {
   init: function () {
     const self = this;
     window.addEventListener("scroll", function () {
-      self.mainElem.classList.add("running");
+      // 스크롤을 할 때마다 계속 clearTimeout이 실행되어 밑에 있는 setTimeout이 실행되지 않는다.
+      // 스크롤을 멈춰야 clear를 안해주기때문에 마지막 스크롤 타이밍에 해당하는 것만 500ms 후에 setTimeout 콜백 실행
+      // 500ms가 지나기 전에 스크롤이 계속 발생하기때문에 setTimeout 콜백이 실행되기 전에 타이머가 clear됨.
+      clearTimeout(self.scrollState);
+
+      if (!self.scrollState) {
+        self.mainElem.classList.add("running");
+      }
+
+      self.scrollState = setTimeout(function () {
+        self.scrollState = false;
+        self.mainElem.classList.remove("running");
+      }, 500);
     });
   },
 };
